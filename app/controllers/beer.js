48da18823b824f47219cc5a151c3eb02b57f1caa
noctoz <noctoz@outlook.com>
2016-07-15 01:53:51 +0200

    Added create recipe to beer

--- app/controllers/beer.js
+++ app/controllers/beer.js
@@ -5,7 +5,16 @@ export default Ember.Controller.extend({
     updateModel() {
       console.log(event.target.value);
       this.model.set(event.target.name, event.target.value);
+    },
+    saveModel() {
       this.model.save();
+    },
+    createRecipe(beerId) {
+      console.log("Create recipe for beer with id " + beerId);
+      var recipe = this.store.createRecord('recipe', {
+        beer: this.model
+      });
+      recipe.save();
     }
   }
 });
