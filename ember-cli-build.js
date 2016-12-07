1c4e9b58293859a9b17e1b154751d8c5223ba03c
Gidlund <johan.gidlund@frostbite.com>
2016-12-07 13:40:09 +0100

    fixed glyphicons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -23,6 +23,9 @@ module.exports = function(defaults) {
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+    destDir: 'fonts'
+  });
 
   return app.toTree();
 };
