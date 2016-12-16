3cb1cd01d5b88ce174dd19d8de53137ed43ada81
noctoz <noctoz@hotmail.com>
2016-12-16 16:58:53 +0100

    Read comments from data

--- app/models/news-comment.js
+++ app/models/news-comment.js
@@ -1,9 +1,12 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
 	author: belongsTo('user'),
 	newsItem: belongsTo('news-item'),
-	content: attr('string')
+	children: hasMany('news-comment', { inverse: 'parent' }),
+	parent: belongsTo('news-comment', { inverse: 'children' }),
+	content: attr('string'),
+	created: attr('date', { defaultValue() { return new Date(); } })
 });
