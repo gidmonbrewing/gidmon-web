0a474d5f245f668ca88ad207acbf47b5f06114ad
noctoz <noctoz@hotmail.com>
2016-12-20 15:23:30 +0100

    Added support for profiles and pictures

---
+++ app/components/file-upload.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import EmberUploader from 'ember-uploader';
+
+export default EmberUploader.FileField.extend({
+	store: Ember.inject.service(),
+	filesDidChange: function (files) {
+		const uploader = EmberUploader.Uploader.create({
+			url: this.get('url'),
+			ajaxSettings: {
+				headers: this.get('store').adapterFor('application').get('headers')
+			}
+		});
+	
+		if (!Ember.isEmpty(files)) {
+			// this second argument is optional and can to be sent as extra data with the upload
+			uploader.upload(files[0], {});
+		}
+	}
+});
