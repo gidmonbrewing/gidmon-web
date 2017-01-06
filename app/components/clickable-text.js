befdd470d8b5583978e73804b9108f5d8566dc51
noctoz <noctoz@outlook.com>
2017-01-06 22:31:42 +0100

    Recipe fixes

--- app/components/clickable-text.js
+++ app/components/clickable-text.js
@@ -1,7 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  click() {
-    alert("DoubleClickableComponent was clicked!");
-  }
+	tagName: 'span',
+	click() {
+		//alert("DoubleClickableComponent was clicked!");
+	},
+	keyUp() {
+		//var value = Number.parseFloat(this.get('element').children[0].innerText);
+		//if (Number.isNaN(value)) {
+		//	return false;
+		//}
+	},
+	keyDown(param) {
+		if (param.keyCode === 13) {
+			var value = Number.parseFloat(this.get('element').children[0].innerText);
+			if (Number.isNaN(value)) {
+				alert("Can only input number");
+			} else {
+				this.set('value', value);
+			}
+			return false;
+		}
+	},
 });
--- app/controllers/recipe.js
+++ app/controllers/recipe.js
@@ -4,6 +4,26 @@ export default Ember.Controller.extend({
 	actions: {
 		saveModel() {
 			this.model.recipe.save();
+			// save all dirty mash entries
+			this.model.recipe.get('mashEntries').then(function (entries) {
+				entries.forEach(function (element) {
+					if (element.get('hasDirtyAttributes')) {
+						element.save();
+					}
+				});
+			});
 		},
-	}
+	},
+	hasUnsavedChanges: Ember.computed('model.recipe.hasDirtyAttributes', 'model.recipe.mashEntries.@each.hasDirtyAttributes', function () {
+		var result = false;
+		if (this.model.recipe.get('hasDirtyAttributes')) {
+			result = true;
+		}
+		this.model.recipe.get('mashEntries').forEach(function (element) {
+			if (element.get('hasDirtyAttributes')) {
+				result = true;
+			}
+		});
+		return result;
+	}),
 });
