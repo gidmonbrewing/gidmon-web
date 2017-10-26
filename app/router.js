73afee2359267bb92886545ca5f870a85e777109
noctoz <noctoz@outlook.com>
2017-10-27 01:07:10 +0200

    Updated to new Ember version

--- app/router.js
+++ app/router.js
@@ -1,32 +1,32 @@
-import Ember from 'ember';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
-    location: config.locationType,
-    rootURL: config.rootURL
+const Router = EmberRouter.extend({
+	location: config.locationType,
+	rootURL: config.rootURL
 });
 
-Router.map(function() {
-  this.route('recipes');
-  this.route('recipe', { path: '/recipe/:recipe_id' });
-  this.route('beers');
-  this.route('beer', { path: '/beer/:beer_id' });
-  this.route('login');
-  this.route('about');
-  this.route('news-item', { path: '/news-item/:news-item_id' }, function() {
-      this.route('comment');
-  });
-  this.route('news', function() {
-      this.route('new');
-      this.route('item', { path: '/:news_id' }, function() {
-          this.route('comment');
-      });
-      this.route('edit');
-  });
-  this.route('profile', { path: '/profile/:profile_id' });
-  this.route('brewing-session', { path: '/brewing-session/:session_id' }, function () {
-    this.route('comment');
-  });
+Router.map(function () {
+	this.route('recipes');
+	this.route('recipe', { path: '/recipe/:recipe_id' });
+	this.route('beers');
+	this.route('beer', { path: '/beer/:beer_id' });
+	this.route('login');
+	this.route('about');
+	this.route('news-item', { path: '/news-item/:news-item_id' }, function () {
+		this.route('comment');
+	});
+	this.route('news', function () {
+		this.route('new');
+		this.route('item', { path: '/:news_id' }, function () {
+			this.route('comment');
+		});
+		this.route('edit');
+	});
+	this.route('profile', { path: '/profile/:profile_id' });
+	this.route('brewing-session', { path: '/brewing-session/:session_id' }, function () {
+		this.route('comment');
+	});
 });
 
 export default Router;
